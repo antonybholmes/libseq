63d46b42114bfd1ba3901b594e6754b204850a54
Antony Holmes <antony@antonyholmes.dev>
2024-12-18 13:43:05 -0500

    fix: Switched to native endian

--- commit.sh
+++ commit.sh
@@ -1,3 +1,39 @@
-git add -A
-git commit -m 'Updates and bug fixes.'
-git push -u origin main
+msg=$1 #"Bug fixes and updates."
+type="fix"
+branch="main"
+
+if [[ -z "${msg}" ]]
+then
+	msg="Bug fixes and updates."
+fi
+
+OPTSTRING="t:m:b:"
+
+while getopts ${OPTSTRING} opt
+do
+	case ${opt} in
+  	t)
+    	type=$OPTARG
+      	;;
+	m)
+    	msg=$OPTARG
+      	;;
+	b)
+      	branch=$OPTARG
+      	;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+echo "${type}: ${msg}"
+echo ${branch}
+
+#pnpm update-version
+
+# commit
+git add -A .
+git commit -m "${type}: ${msg}"
+git push -u origin ${branch}
